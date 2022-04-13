7a9ee7e91308ab7672cd3fc986a4677cd9ddcaba
sokhay-try <sokhay-try@users.noreply.github.com>
2022-04-13 12:23:04 +0000

    deploy: 5731f297c0734b1854e8d6f621db619e79851a67

---
+++ precache-manifest.04ef27cda4c9b4d341a10a45ee1520f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff6c7a468be976e1660e01e23cec0cb",
+    "url": "https://sokhay-try.github.io/task-management-nestjs/index.html"
+  },
+  {
+    "revision": "88b9e44597ca44559254",
+    "url": "https://sokhay-try.github.io/task-management-nestjs/static/css/main.221589fe.chunk.css"
+  },
+  {
+    "revision": "824bd5f1fc29ee01ed19",
+    "url": "https://sokhay-try.github.io/task-management-nestjs/static/js/2.5aec08c2.chunk.js"
+  },
+  {
+    "revision": "88b9e44597ca44559254",
+    "url": "https://sokhay-try.github.io/task-management-nestjs/static/js/main.d67461e6.chunk.js"
+  },
+  {
+    "revision": "4f8ca6dbe3dd520851ac",
+    "url": "https://sokhay-try.github.io/task-management-nestjs/static/js/runtime~main.123aa176.js"
+  }
+]);
